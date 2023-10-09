aa0312096cf766a59122ec1f0990d08b4080db08
gonzalomadariaga1 <gonzalo.madariaga@alumnos.uv.cl>
2023-10-08 22:30:17 -0300

    configuracion final

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,155 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+        <title>@yield('title') - {{ config('app.name', 'Laravel') }}</title>
+        
+        {{--Jquery--}}
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    
+    
+        <script src="{{asset('dist/js/demo-theme.min.js?1674944402')}}"></script>
+    
+        <!-- Theme CSS files -->
+        <link rel="stylesheet" href="{{asset('dist/css/tabler.min.css')}}">
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta3/css/bootstrap-select.css">
+    
+        <style>
+          @import url('https://rsms.me/inter/inter.css');
+          :root {
+              --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+          }
+          body {
+              font-feature-settings: "cv03", "cv04", "cv11";
+          }
+          .cssButton{
+            background-color: #206bc4 !important;
+          }
+        </style>
+    
+        {{-- Scripts CSS --}}
+        {{-- Bootstrap Icons --}}
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    
+        {{-- DataTables --}}
+        <link href="https://cdn.datatables.net/v/bs5/dt-1.13.6/datatables.min.css" rel="stylesheet">
+
+        {{-- BS Stepper --}}
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bs-stepper/dist/css/bs-stepper.min.css">
+    
+
+        @stack('css-styles')
+    
+    </head>
+    <body>
+        <div class="page">
+            <!-- Navbar -->
+            <header class="navbar navbar-expand-md d-print-none" >
+              <div class="container-xl">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu" aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
+                    <a href="{{route('home')}}" class="logo d-flex align-items-center">
+                        <span class="d-none d-lg-block">{{ config('app.name', 'Laravel') }}</span>
+                    </a>
+                </h1>
+                <div class="navbar-nav flex-row order-md-last">
+
+                  <div class="d-none d-md-flex me-2">
+                    <a href="?theme=dark" class="nav-link px-0 hide-theme-dark" title="Enable dark mode" data-bs-toggle="tooltip" data-bs-placement="bottom">
+                      <!-- Download SVG icon from http://tabler-icons.io/i/moon -->
+                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" /></svg>
+                    </a>
+                    <a href="?theme=light" class="nav-link px-0 hide-theme-light" title="Enable light mode" data-bs-toggle="tooltip" data-bs-placement="bottom">
+                      <!-- Download SVG icon from http://tabler-icons.io/i/sun -->
+                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" /></svg>
+                    </a>
+                  </div>
+
+                  <div class="nav-item dropdown">
+                    @guest
+                        <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
+                            <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000m2.png)"></span>
+                            <div class="d-none d-xl-block ps-2">
+                            <div>{{ __('Login') }} / {{ __('Register') }}</div>
+                            </div>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                            @if (Route::has('login'))
+                                <a href="{{ route('login') }}" class="dropdown-item">{{ __('Login') }}</a>
+                            @endif
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="dropdown-item">{{ __('Register') }}</a>
+                            @endif
+                        </div>
+                    @else
+                        <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
+                            <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000m2.png)"></span>
+                            <div class="d-none d-xl-block ps-2">
+                                    <div>{{Auth::user()->name}}</div>
+                                    <div class="mt-1 small text-muted">
+                                        @if ( Auth::user()->getRoleNames()->isEmpty()  )
+                                        Sin rol
+                    
+                                        @else
+                                        @foreach (Auth::user()->getRoleNames() as $rol )
+                                            {{ $rol}} 
+                                        @endforeach
+                                        @endif
+                                    </div>
+                            </div>
+                        </a>
+                    
+                        <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    @endguest    
+
+                  </div>
+                </div>
+              </div>
+            </header>
+
+            @includeWhen(Auth::check(),'layouts.menu')
+
+
+            <div class="page-wrapper">
+              <!-- Page body -->
+              <div class="page-body">
+                <div class="container-xl d-flex flex-column justify-content-center">
+                    @yield('content')
+                </div>
+              </div>
+              @include('layouts.footer')
+
+            </div>
+          </div>
+        <!-- Tabler Core -->
+        <script src="{{asset('dist/js/tabler.min.js')}}"></script>
+        <script src="{{asset('dist/js/demo-theme.min.js')}}"></script>
+        {{-- Bootstrap Select --}}
+        <script src="{{asset('dist/js/bs.js')}}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/i18n/defaults-es_ES.min.js"></script>
+
+        {{-- DataTables --}}
+        <script src="https://cdn.datatables.net/v/bs5/dt-1.13.6/datatables.min.js"></script>
+        
+        {{-- Bs Stepper --}}
+        <script src="	https://cdn.jsdelivr.net/npm/bs-stepper/dist/js/bs-stepper.min.js"></script>
+
+    
+        @include('sweetalert::alert')
+        @yield('code_js')
+    </body>
